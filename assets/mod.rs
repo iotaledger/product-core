524426cffd10d764de2efc6f97fde0c10a2bd027
chrisgitiota <christof.gerritsma@iota.org>
2025-04-15 19:32:52 +0200

    Move selected files from identity_iota_core/src/rebased to core/src folder after git subtree split and delete all other files

--- assets/mod.rs
+++
@@ -1,8 +0,0 @@
-// Copyright 2020-2024 IOTA Stiftung
-// SPDX-License-Identifier: Apache-2.0
-
-mod asset;
-mod public_available_vc;
-
-pub use asset::*;
-pub use public_available_vc::*;
--- iota/mod.rs
+++
@@ -1,5 +0,0 @@
-// Copyright 2020-2024 IOTA Stiftung
-// SPDX-License-Identifier: Apache-2.0
-
-pub(crate) mod types;
-pub(crate) mod well_known_networks;
