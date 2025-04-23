d82a0e9accf1f5c78c61df534ee43c847b81974b
Yasir <yasir@shariff.dev>
2025-04-23 11:59:05 +0300

    Refactor transaction module: move files into dedicated directory structure

--- product_common/src/lib.rs
+++ product_common/src/lib.rs
@@ -1,7 +1,6 @@
+pub mod core_client;
 pub mod objects;
 pub mod test_common;
-pub mod transaction;
-pub mod transaction_builder;
 pub mod utils;
 pub mod well_known_networks;
-pub mod core_client;
+pub mod transaction;
