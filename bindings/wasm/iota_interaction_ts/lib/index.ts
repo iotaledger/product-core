624fd3f9cf1ec303c2e62caf9407f9a29d1b2a96
Yasir <yasir@shariff.dev>
2025-05-09 11:42:25 +0300

    chore: add transaction internal

--- bindings/wasm/iota_interaction_ts/lib/index.ts
+++ bindings/wasm/iota_interaction_ts/lib/index.ts
@@ -2,5 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export * from "~iota_interaction_ts";
-export * as iota_client_helpers from "./iota_client_helpers";
-export * as core_client from "./core_client"
+export *  from "./iota_client_helpers";
+export *  from "./core_client";
+export * from "./transaction_internal"
