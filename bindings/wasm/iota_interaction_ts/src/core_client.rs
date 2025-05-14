460aa49d50accce07e85c8cb5ade88cef5ddc0f1
Yasir <yasir@shariff.dev>
2025-05-14 10:41:36 +0300

    feat: add Apache-2.0 license header to core_client.rs

--- bindings/wasm/iota_interaction_ts/src/core_client.rs
+++ bindings/wasm/iota_interaction_ts/src/core_client.rs
@@ -1,3 +1,6 @@
+// Copyright 2020-2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 use wasm_bindgen::prelude::wasm_bindgen;
 
 use crate::bindings::{WasmIotaClient, WasmTransactionSigner};
