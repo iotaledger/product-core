d9db8500d5b899fc6bab01430ae61a6bc3732fc5
chrisgitiota <christof.gerritsma@iota.org>
2025-08-11 13:41:17 +0200

    fix fmt issue

--- product_common/src/move_history_manager.rs
+++ product_common/src/move_history_manager.rs
@@ -99,24 +99,28 @@ impl PackageRegistry {
 ///
 /// ```rust
 /// use std::path::PathBuf;
+///
 /// use product_common::move_history_manager::MoveHistoryManager;
 ///
 /// fn main() {
-///     let move_lock_path = "../<Move-Package-Name>/Move.lock";
-///     println!("[build.rs] move_lock_path: {move_lock_path}");
-///     let move_history_path = "../<Move-Package-Name>/Move.history.json";
-///     println!("[build.rs] move_history_path: {move_history_path}");
+///   let move_lock_path = "../<Move-Package-Name>/Move.lock";
+///   println!("[build.rs] move_lock_path: {move_lock_path}");
+///   let move_history_path = "../<Move-Package-Name>/Move.history.json";
+///   println!("[build.rs] move_history_path: {move_history_path}");
 ///
-///     MoveHistoryManager::new(
-///         &PathBuf::from(move_lock_path),
-///         &PathBuf::from(move_history_path),
-///         // Use `Some(vec![])` instead of `None`, if you don't want to ignore `localnet`
-///         None,
-///     ).manage_history_file(|message| { println!("[build.rs] {}", message); })
-///         .expect("Successfully managed Move history file");
+///   MoveHistoryManager::new(
+///     &PathBuf::from(move_lock_path),
+///     &PathBuf::from(move_history_path),
+///     // Use `Some(vec![])` instead of `None`, if you don't want to ignore `localnet`
+///     None,
+///   )
+///   .manage_history_file(|message| {
+///     println!("[build.rs] {}", message);
+///   })
+///   .expect("Successfully managed Move history file");
 ///
-///     // Tell Cargo to rerun this build script if the Move.lock file changes.
-///     println!("cargo::rerun-if-changed={move_lock_path}");
+///   // Tell Cargo to rerun this build script if the Move.lock file changes.
+///   println!("cargo::rerun-if-changed={move_lock_path}");
 /// }
 /// ```
 ///
