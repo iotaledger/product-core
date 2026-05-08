1e1b7ce50eb6343e8137818d117d86a2b5b9e983
Christof Gerritsma <christof.gerritsma@iota.org>
2026-05-08 09:35:24 +0200

    Upstream merge to iota v1.22.1 (#116)
    Summary:
    - [ ] tokio version update to: -
    - [ ] fastcrypto version update to rev = "-"
    - [x] iota-sdk-types (`https://github.com/iotaledger/iota-rust-sdk.git`) update to rev = 7c1434aabb2e7b11681722edaf1a51ac52941b84
    - [x] changes in iota_interaction/src/sdk_types
    - [ ] iota_interaction_ts: new peerDep. version for @iota/iota-sdk: _
    - [x] pin all iota.git dependencies to `tag = "v1.22.1"`

--- iota_interaction/src/sdk_types/iota_types/digests.rs
+++ iota_interaction/src/sdk_types/iota_types/digests.rs
@@ -494,18 +494,6 @@ impl fmt::Debug for ObjectDigest {
     }
 }
 
-
-/// A digest of a ZkLoginInputs, which commits to the signatures as well as the
-/// tx.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct ZKLoginInputsDigest(Digest);
-
-impl ZKLoginInputsDigest {
-    pub const fn new(digest: [u8; 32]) -> Self {
-        Self(Digest::new(digest))
-    }
-}
-
 /// MoveAuthenticatorDigest is the hash (digest) of the `GenericSignature`
 /// payload when the transaction uses a `MoveAuthenticator` as its signature
 /// scheme. It is evaluated during the authentication phase of a transaction and
